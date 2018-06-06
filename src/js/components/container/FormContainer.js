c5b8da47c504f500bfa9ad859b106000b5836b84
Max Wang <mwang@1080agile.com>
2018-06-06 11:45:44 +1000

    update input event handler

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -13,7 +13,7 @@ class FormContainer extends Component {
     }
 
     handleChange(event) {
-        this.setState({ [event.target.id]: event.target.value });
+        this.setState({ title: event.target.value });
     }
 
     render() {
